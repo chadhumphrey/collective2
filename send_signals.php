373040ca0d86e71474bfd423543ea7c5efe74d87
chadhumphrey <chumphrey@usbr.gov>
2017-02-08 12:16:13 -0700

    frist commit

---
+++ send_signals.php
@@ -0,0 +1,15 @@
+<?php require '../vendor/autoload.php';
+
+use \Curl\Curl;
+
+$curl = new Curl();
+$curl->get('https://api.collective2.com/world/apiv3/getSystemRoster');
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
+
+// https://packagist.org/packages/php-curl-class/php-curl-class
